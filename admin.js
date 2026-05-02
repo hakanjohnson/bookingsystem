d6c61e6b517f8573d1066cca24f49016dfa519b6
Hakan Johnson <hakan.johnson@aol.de>
2026-05-02 22:07:52 +0200

    first css changes

--- admin.js
+++ admin.js
@@ -1,55 +1,85 @@
 
-  //SETUP*********
+//SETUP*********
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+import { getFirestore, collection, onSnapshot } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-  import { getFirestore, collection, onSnapshot } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
-  import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDXJUifQRtWZAFqg5Z_RLp-RmIuKp_jQpU",
+  authDomain: "buchungssystem-75196.firebaseapp.com",
+  projectId: "buchungssystem-75196",
+  storageBucket: "buchungssystem-75196.firebasestorage.app",
+  messagingSenderId: "903591657810",
+  appId: "1:903591657810:web:b7c1397ae91c6e517b7a8b",
+  measurementId: "G-7XNM9T62CX"
+};
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDXJUifQRtWZAFqg5Z_RLp-RmIuKp_jQpU",
-    authDomain: "buchungssystem-75196.firebaseapp.com",
-    projectId: "buchungssystem-75196",
-    storageBucket: "buchungssystem-75196.firebasestorage.app",
-    messagingSenderId: "903591657810",
-    appId: "1:903591657810:web:b7c1397ae91c6e517b7a8b",
-    measurementId: "G-7XNM9T62CX"
-  };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "login.html";
+  }
+});
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
+onAuthStateChanged(auth, (user ) => {
+  if(user) {
+    const email = user.email;
+    document.getElementById("greet").textContent = "Hallo, " + email;
+  }
+})
 
-  const auth = getAuth();
-  onAuthStateChanged(auth, (user) => {
-    if (!user) {
-      window.location.href = "login.html";
-    }
+
+//LOGIC*********
+
+//Gebuchte Termine anzeigen
+const container = document.getElementById("termine");
+onSnapshot(collection(db, "termine"), (snapshot) => {
+  container.innerHTML = "";
+  snapshot.forEach((doc) => {
+      const data = doc.data();
+      const div = document.createElement("div");
+      div.style.border = "3px solid #3bec38";
+      div.style.borderRadius = "6px";
+      div.style.margin = "10px";
+      div.style.padding = "10px";
+
+      div.innerHTML = `
+      <strong>Name:</strong> ${data.name} <br>
+      <strong>Datum:</strong> ${data.date} <br>
+      <strong>Zeit:</strong> ${data.time} <br>
+      `;
+      container.appendChild(div);
   });
+});
 
 
-  const container = document.getElementById("termine");
-  //LOGIC*********
-  onSnapshot(collection(db, "termine"), (snapshot) => {
-    container.innerHTML = "";
-    snapshot.forEach((doc) => {
-        const data = doc.data();
-        const div = document.createElement("div");
-        div.style.border = "1px solid #cccccc";
-        div.style.margin = "10px";
-        div.style.padding = "10px";
+//Kurse anzeigen
+const containerKurse = document.getElementById("kurse");
+onSnapshot(collection(db, "kurse"), (snapshot) => {
+  containerKurse.innerHTML = "";
+  snapshot.forEach((doc) => {
+      const data = doc.data();
+      const div = document.createElement("div");
+      div.style.border = "3px solid #253bb9";
+      div.style.borderRadius = "6px";
+      div.style.margin = "10px";
+      div.style.padding = "10px";
 
-        div.innerHTML = `
-        <strong>Name:</strong> ${data.name} <br>
-        <strong>Datum:</strong> ${data.date} <br>
-        <strong>Zeit:</strong> ${data.time} <br>
-        `;
-        container.appendChild(div);
-    });
+      div.innerHTML = `
+      <strong>Termin:</strong> ${data.title} <br>
+      <strong>Datum:</strong> ${data.date} <br>
+      <strong>Zeit:</strong> ${data.time} <br>
+      <strong>Plätze:</strong> ${data.kapazität} <br>
+      `;
+      containerKurse.appendChild(div);
   });
-  
+});
 
--- buchung.app.js
+++ buchung.app.js
@@ -1,54 +1,33 @@
 
-  //SETUP*********
+//SETUP*********
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-analytics.js";
-  import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-analytics.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDXJUifQRtWZAFqg5Z_RLp-RmIuKp_jQpU",
-    authDomain: "buchungssystem-75196.firebaseapp.com",
-    projectId: "buchungssystem-75196",
-    storageBucket: "buchungssystem-75196.firebasestorage.app",
-    messagingSenderId: "903591657810",
-    appId: "1:903591657810:web:b7c1397ae91c6e517b7a8b",
-    measurementId: "G-7XNM9T62CX"
-  };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+apiKey: "AIzaSyDXJUifQRtWZAFqg5Z_RLp-RmIuKp_jQpU",
+authDomain: "buchungssystem-75196.firebaseapp.com",
+projectId: "buchungssystem-75196",
+storageBucket: "buchungssystem-75196.firebasestorage.app",
+messagingSenderId: "903591657810",
+appId: "1:903591657810:web:b7c1397ae91c6e517b7a8b",
+measurementId: "G-7XNM9T62CX"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-  //LOGIC*********
+//LOGIC*********
 
-  window.book = async function () {
-    const date = document.getElementById("date").value;
-    const time = document.getElementById("time").value;
-    const name = document.getElementById("name").value;
-
-    if (!date || !time || !name) {
-        alert("Bitte alle Felder ausfüllen");
-        return;
-    }
-
-    try {
-        await addDoc(collection(db, "termine"), {
-            date: date,
-            time: time,
-            name: name,
-            createAt: new Date()
-        });
-        alert("Termin wurde erfolgreich gebucht");
-    }
-    catch (error) {
-        console.error("Fehler:", error.code, error.message);
-        alert(error.message);
-    }
-  };
-
+const container = document.getElementById("app");
+const select = document.createElement("select");
+select.id = "kursAuswahl"
+container.appendChild(select);
 
